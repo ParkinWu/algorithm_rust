18be86e484e3213f865db5a4b03655332945a232
parkinwu <snowy_way@163.com>
2016-09-02 15:09:24 +0800

    haffman tree

--- tree/src/tree/binary_tree.rs
+++ tree/src/tree/binary_tree.rs
@@ -1,3 +1,25 @@
-pub fn say_hi() {
-    println!("hello world");
+pub type Link = Option<Box<Node>>;
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub pow: f32,
+    pub key: Option<char>,
+    pub left: Link,
+    pub right: Link,
+}
+
+impl Drop for Node {
+    fn drop(&mut self) {
+//        println!("self.left = {:#?}", self.left);
+    }
+}
+
+impl Node {
+    pub fn new(key: Option<char>, pow: f32) -> Self {
+        Node {
+            pow: pow,
+            key: key,
+            left: None,
+            right: None,
+        }
+    }
 }
